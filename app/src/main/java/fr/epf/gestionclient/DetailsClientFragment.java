30d1e6cd06b036f3dd6485b546d36f07f4e45574
StephaneGcrd <stephane.gcrd@gmail.com>
2018-03-24 10:12:48 +0100

    bindview fragment

--- app/src/main/java/fr/epf/gestionclient/DetailsClientFragment.java
+++ app/src/main/java/fr/epf/gestionclient/DetailsClientFragment.java
@@ -3,38 +3,29 @@ package fr.epf.gestionclient;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import butterknife.BindView;
 import fr.epf.gestionclient.model.Client;
 
 public class DetailsClientFragment extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details_client);
-
-        Intent intent = getIntent();
-
-        int idClient = intent.getIntExtra(ListClientsFragment.ID_CLIENT, 0);
-        Client client = Client.getClients().get(idClient);
-
-        TextView id_client = (TextView)findViewById(R.id.lastname_textview);
-
-
-        id_client.setText(client.getLastname());
+    @BindView(R.id.lastname_textview)
+    TextView lastnameTextView;
+        
+    public void updateClient(int id){
+        Client client = Client.getClients().get(id);
+        lastnameTextView.setText(client.getLastname());
 
     }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        View view = inflater.inflate(R.layout.activity_details_client, container, true);
+        View view = inflater.inflate(R.layout.fragment_details_client, container, true);
         return view;
     }
 }
